c59eaa4d1916510cb5a4341bc7b0c21eda18f3f8
Koufuchi <jackkao8752@gmail.com>
2023-12-15 09:21:37 +0800

    feat：add adapter example

---
+++ Adapter.py
@@ -0,0 +1,47 @@
+class Target:
+    def request(self) -> str:
+        return "Target: The default target's behavior."
+
+
+class Adaptee:
+    def specific_request(self) -> str:
+        return ".eetpadA eht fo roivaheb laicepS"
+
+
+class ClassAdapter(Target, Adaptee):
+    def request(self) -> str:
+        return f"Adapter: (TRANSLATED) {self.specific_request()[::-1]}"
+
+
+class ObjectAdapter(Target):
+    def __init__(self, adaptee: Adaptee) -> None:
+        self.adaptee = adaptee
+
+    def request(self) -> str:
+        return f"Adapter: (TRANSLATED) {self.adaptee.specific_request()[::-1]}"
+
+
+
+def client_code(target: Target) -> None:
+    print(target.request(), end="")
+
+
+if __name__ == "__main__":
+    print("Client: I can work just fine with the Target objects:")
+    target = Target()
+    client_code(target)
+    print("\n")
+
+    adaptee = Adaptee()
+    print("Client: The Adaptee class has a weird interface. "
+          "See, I don't understand it:")
+    print(f"Adaptee: {adaptee.specific_request()}", end="\n\n")
+
+    print("Client: But I can work with it via the ClassAdapter:")
+    adapter = ClassAdapter()
+    client_code(adapter)
+
+    print("\n")
+    print("Client: I can also work with it via the ObjectAdapter:")
+    adapter = ObjectAdapter(adaptee)
+    client_code(adapter)
